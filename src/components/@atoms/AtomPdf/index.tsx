f1fd3e41daad17588b6d3c912b1220e977f5babd
Yuborama <davidjohan2@hotmail.com>
2022-01-05 00:15:34 -0500

    fix: pdf brick

--- src/components/@atoms/AtomPdf/index.tsx
+++ src/components/@atoms/AtomPdf/index.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { FC } from 'react';
 import {
   Page,
-  Text,
   View,
   Document,
   StyleSheet,
@@ -32,24 +32,15 @@ type AtomPdfProps = {
 
 export const AtomPdf: FC<AtomPdfProps> = (props) => {
   const { images } = props;
-  // console.log(`images que entran`, images);
   return (
     <Document>
-      {images.length > 0 ? (
-        images.map((image, index) => (
-          <Page key={`${index + 1}`} size="A4" style={styles.page}>
-            <View style={styles.section} key={index}>
-              <Image src={image} style={styles.image} />
-            </View>
-          </Page>
-        ))
-      ) : (
-        <Page size="A4" style={styles.page}>
-          <View style={styles.section}>
-            <Text>No hay imagenes</Text>
+      {images?.map((image, index) => (
+        <Page key={`${index + 1}`} size="A4" style={styles.page}>
+          <View style={styles.section} key={index}>
+            <Image src={image} style={styles.image} />
           </View>
         </Page>
-      )}
+      ))}
     </Document>
   );
 };
@@ -59,23 +50,7 @@ type DocumentProps = {
 };
 const DownloadPdf: FC<DocumentProps> = (props) => {
   const { images } = props;
-  const [FechtImage, setFechtImage] = useState<string[]>([]);
-  useEffect(() => {
-    const prueba = async () => {
-      const allimages = await Promise.all(
-        images.map((image) =>
-          fetch(image)
-            .then((res) => res.blob())
-            .then((blob) => URL.createObjectURL(blob))
-        )
-      );
-      setFechtImage(allimages);
-    };
-    prueba();
-  }, [images]);
-  // console.log(`images que entran en donwload`, images);
-  // console.log(`images fechtimage`, FechtImage);
-  const Document = <AtomPdf images={FechtImage} />;
+  const Document = <AtomPdf images={images} />;
 
   const [instance] = usePDF({ document: Document });
   if (instance.loading) return <div>Loading ...</div>;
@@ -83,9 +58,8 @@ const DownloadPdf: FC<DocumentProps> = (props) => {
     return <div>Something went wrong:{`${instance.error}`}</div>;
   return (
     <>
-      <AtomPdf images={FechtImage} />;
       <a href={`${instance.url}`} download="test.pdf">
-        Download
+        Descargar
       </a>
     </>
   );

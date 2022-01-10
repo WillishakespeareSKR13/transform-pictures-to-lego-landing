7e6769f6368f57a95e5b8692170dd72730638f3e
Yuborama <davidjohan2@hotmail.com>
2022-01-09 21:07:05 -0500

    fix: document void

--- src/components/@atoms/AtomPdf/index.tsx
+++ src/components/@atoms/AtomPdf/index.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
   Page,
   View,
   Document,
   StyleSheet,
-  usePDF,
-  Image
+  // usePDF,
+  Image,
+  PDFDownloadLink
 } from '@react-pdf/renderer';
 
 // Create styles
@@ -48,21 +49,40 @@ export const AtomPdf: FC<AtomPdfProps> = (props) => {
 type DocumentProps = {
   images: string[];
 };
+
 const DownloadPdf: FC<DocumentProps> = (props) => {
   const { images } = props;
-  const Document = <AtomPdf images={images} />;
-
-  const [instance] = usePDF({ document: Document });
-  if (instance.loading) return <div>Loading ...</div>;
-  if (instance.error)
-    return <div>Something went wrong:{`${instance.error}`}</div>;
+  const [Document, setDocument] = useState(<AtomPdf images={images} />);
+  useEffect(() => {
+    setDocument(<AtomPdf images={images} />);
+  }, [images]);
   return (
-    <>
-      <a href={`${instance.url}`} download="test.pdf">
-        Descargar
-      </a>
-    </>
+    <div>
+      <PDFDownloadLink document={Document} fileName="somename.pdf">
+        {({ loading }) => (loading ? 'cargando documento...' : 'Descargar!')}
+      </PDFDownloadLink>
+    </div>
   );
 };
+
+// const DownloadPdf2: FC<DocumentProps> = (props) => {
+//   const { images } = props;
+//   const [Document, setDocument] = useState(<AtomPdf images={images} />);
+//   console.log(`Download`, images);
+//   useEffect(() => {
+//     setDocument(<AtomPdf images={images} />);
+//   }, [images]);
+//   const [instance] = usePDF({ document: Document });
+//   if (instance.loading) return <div>Loading ...</div>;
+//   if (instance.error)
+//     return <div>Something went wrong:{`${instance.error}`}</div>;
+//   return (
+//     <>
+//       <a href={`${instance.url}`} download="test.pdf">
+//         Descargar
+//       </a>
+//     </>
+//   );
+// };
 
 export default DownloadPdf;

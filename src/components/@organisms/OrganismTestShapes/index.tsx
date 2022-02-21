9788671aa64c9287fe01cab5fccac76c0b93a687
Yuborama <davidjohan2@hotmail.com>
2022-02-20 21:14:29 -0500

    test pintura

---
+++ src/components/@organisms/OrganismTestShapes/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC, useEffect, useRef } from 'react';
+
+const OrganismTestShapes: FC = () => {
+  //   console.log(Caman);
+  return <div></div>;
+};
+export default OrganismTestShapes;
